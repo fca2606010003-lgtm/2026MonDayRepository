7d95e72bbd754f4cbca383b24e5ed2d8a3ee6f9f
fca2606010003-lgtm <fca2606010003@edu.fca.ac.jp>
2026-07-13 11:02:16 +0900

    更新

---
+++ 20260713/20260707_Prac1_iida.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include"20260707_H1_iida.h"
+using namespace std;
+
+/*10 個の数値を入力する。
+入力された数値を偶数（even）と奇数（odd）に分類して表示する。
+数字を入れる配列は関数にして配列を渡しましょう。*/
+
+int main(void)
+{
+	//配列
+	int arry[INDEX] = {};
+	//
+	cout << "10個の整数を入力する。\n";
+	//
+	AddArray(arry, INDEX);
+	//
+	cout << "偶数" << endl;
+
+	for (int i = 0; i < INDEX; i++)
+	{
+		if (arry[i] % 2 == 0)
+		{
+			cout << arry[i] << "\n";
+		}
+	}
+	cout << "奇数:" << endl;
+	for (int i = 0; i < INDEX; i++)
+	{
+		if (arry[i%] % 2 != 0)
+		{
+			cout << arry[i] << "\n";
+		}
+	}
+
+	return 0;
+}
